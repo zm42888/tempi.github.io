f575f4a5974a7db9fc9548c62a39c9fea86febe1
Zubair <zm42888@gmail.com>
2023-12-06 18:44:44 +0500

    new

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "ac41d82c9541ce3c85ce7c3cf8b371e7",
-"index.html": "6ae11475c6f8d5511d1c9ceb7544d242",
-"/": "6ae11475c6f8d5511d1c9ceb7544d242",
-"main.dart.js": "86a9a59683944ead35a40f29a38bc9c0",
+"index.html": "d75de528121c5bc446c59cc89a58b740",
+"/": "d75de528121c5bc446c59cc89a58b740",
+"main.dart.js": "2a11cbbed4353adc49d7822c9769c78c",
 "flutter.js": "7a1d76a8729d1add7a6ce2a4b4db043f",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
